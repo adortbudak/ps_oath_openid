2c2dcd4d53c3e6a1ea0ff8c3584cd124ab0fff68
Alper Dortbudak <adortbudak@outlook.com>
2017-02-07 14:18:59 -0600

    first commit

---
+++ TripGallery.AngularClient/common/tokenContainer.js
@@ -0,0 +1,44 @@
+﻿(function() {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("tokenContainer",
+            [tokenContainer]);
+
+    function tokenContainer() {
+        var container = {
+            token: ""
+        };
+
+        var setToken = function(token) {
+            container.token = token;
+        };
+
+        var getToken = function () {
+           // debugger;
+            if (container.token === "") {
+                if (localStorage.getItem("access_token") === null) {
+                    var url =
+                        "https://localhost:44317/identity/connect/authorize?" +
+                            "client_id=tripgalleryimplicit&" +
+                            "redirect_uri=" +
+                            encodeURI(window.location.protocol + "//" + window.location.host + "/callback.html") +
+                            "&" +
+                            "response_type=token&" +
+                            "scope=gallerymanagement";
+
+                    window.location = url;
+
+                } else {
+                    setToken(localStorage["access_token"]);
+                }
+            }
+            return container;
+        };
+
+        return {
+            getToken: getToken
+        };
+    };
+})();
---
+++ TripGallery.MVCClient/Helpers/TripGalleryHttpClient.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using IdentityModel.Client;
+
+namespace TripGallery.MVCClient.Helpers
+{
+    public static class TripGalleryHttpClient
+    {
+
+        public static HttpClient GetClient()
+        { 
+            HttpClient client = new HttpClient();
+
+            //var accessToken = RequestAccessTokenClientCredentials();
+
+            //client.SetBearerToken(accessToken);
+
+            var accessToken = RequestAccessTokenAuthorizationCode();
+
+            if (accessToken != null)
+            {
+                client.SetBearerToken(accessToken);
+            }
+            
+           
+            client.BaseAddress = new Uri(Constants.TripGalleryAPI);
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            return client;
+        }
+
+        private static string RequestAccessTokenAuthorizationCode()
+        {
+            var cookie = HttpContext.Current.Request.Cookies.Get("TripGalleryCookie");
+            if (cookie?["access_token"] != null)
+            {
+                return cookie["access_token"];
+            }
+
+            var authorizeRequest = new IdentityModel.Client.AuthorizeRequest(
+                TripGallery.Constants.TripGallerySTSAuthorizationEndpoint);
+
+            var state = HttpContext.Current.Request.Url.OriginalString;
+
+            var url = authorizeRequest.CreateAuthorizeUrl("tripgalleryauthcode", "code", "gallerymanagement",
+                TripGallery.Constants.TripGalleryMVCSTSCallback, state);
+
+            HttpContext.Current.Response.Redirect(url);
+
+            return null;
+        }
+
+        private static string RequestAccessTokenClientCredentials()
+        {
+            var cookie = HttpContext.Current.Request.Cookies.Get("TripGalleryCookie");
+
+            if (cookie?["access_token"] != null)
+            {
+                return cookie["access_token"];
+            }
+
+            var tokenClient = new TokenClient(
+                TripGallery.Constants.TripGallerySTSTokenEndpoint,
+                "tripgalleryclientcredentials",
+                TripGallery.Constants.TripGalleryClientSecret);
+
+            var tokenResponse = tokenClient.RequestClientCredentialsAsync("gallerymanagement").Result;
+
+            TokenHelper.DecodeAndWrite(tokenResponse.AccessToken);
+
+            HttpContext.Current.Response.Cookies["TripGalleryCookie"]["access_token"] = tokenResponse.AccessToken;
+
+            return tokenResponse.AccessToken;
+        }
+    }
+}
